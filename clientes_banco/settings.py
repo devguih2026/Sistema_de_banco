1b23a583eab6ef4cbcf9c577cb825ea962c62dfb
Guilherme Fonseca <guilhermeflor18102002@gmail.com>
2026-05-02 17:36:57 -0300

    Atualização de idioma, time zone e apps instalados

--- clientes_banco/settings.py
+++ clientes_banco/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'django_filters',
+    'rest_framework_simplejwt',
     'sistema',
 ]
 
@@ -138,9 +139,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
